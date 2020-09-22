93d8801e67689668926cac6746678b6d12b34b18
Majid Tooranisama <mtooranisama5070@conestogac.on.ca>
2020-09-22 14:27:08 -0400

    About image added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-export default [
+export default [{"title":"About", "fname":"about.md", "specialImage":"/img/menagerie.png"},
 {"title":"index", "fname":"index.md"},
-{"title":"About", "fname":"about.md", "specialImage":"https://p0.pikist.com/photos/315/635/cat-kittens-small-pet-kitten-cute-charming-sweet-playful.jpg"},
-{"title":"Contact", "fname":"contact.md"}
-];
+{"title":"Contact", "fname":"contact.md"}];
