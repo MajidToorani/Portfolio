03829ccd047231f5734c00b8a27493f6a1c62946
Majid Tooranisama <mtooranisama5070@conestogac.on.ca>
2020-09-27 15:27:58 -0400

    fixed index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 export default [
-{"title":"index", "fname":"index.md"},
+{"title":"Index", "fname":"index.md"},
 {"title":"About", "fname":"about.md", "specialImage":"/img/menagerie.png"},
 {"title":"Contact", "fname":"contact.md"}];
