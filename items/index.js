30b9889df06e30f0bc89a7e8e125aed78cc75a61
Majid Tooranisama <mtooranisama5070@conestogac.on.ca>
2020-09-27 14:08:36 -0400

    Image address fixed

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "title":"Website development", "fname":"website_dev.md", "specialImage":"/img/portfolio-1.JPG"},
-{"id": "item2", "title":"Mobile UI Prototyping", "fname":"mobile_ui.md", "specialImage":"/img/portfolio-2.JPG"},
-{"id": "item3", "title":"C# Programming", "fname":"c_programming.md", "specialImage":"/img/portfolio-3.JPG"},
-{"id": "item4", "title":"Database development", "fname":"database_dev.md", "specialImage":"/img/portfolio-4.JPG"}];
+export default [{"id": "item1", "title":"Website Development", "fname":"website_dev.md", "specialImage":"../img/portfolio-1.JPG"},
+{"id": "item2", "title":"Mobile UI Prototyping", "fname":"mobile_ui.md", "specialImage":"../img/portfolio-2.JPG"},
+{"id": "item3", "title":"C# Programming", "fname":"c_programming.md", "specialImage":"../img/portfolio-3.JPG"},
+{"id": "item4", "title":"Database Development", "fname":"database_dev.md", "specialImage":"../img/portfolio-4.JPG"}];
