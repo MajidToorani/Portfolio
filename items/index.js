02f7d62046916caf953d321010b2d8d96a3ec588
Majid Tooranisama <mtooranisama5070@conestogac.on.ca>
2020-09-27 18:31:22 -0400

    deleted titles

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "title":"Website", "fname":"website_dev.md", "specialImage":"/img/portfolio-1.jpg"},
-{"id": "item2", "title":"Mobile", "fname":"mobile_ui.md", "specialImage":"/img/portfolio-2.jpg"},
-{"id": "item3", "title":"C#", "fname":"c_programming.md", "specialImage":"/img/portfolio-3.jpg"},
-{"id": "item4", "title":"Database", "fname":"database_dev.md", "specialImage":"/img/portfolio-4.jpg"}];
+export default [{"id": "item1", "fname":"website_dev.md", "specialImage":"/img/portfolio-1.jpg"},
+{"id": "item2", "fname":"mobile_ui.md", "specialImage":"/img/portfolio-2.jpg"},
+{"id": "item3", "fname":"c_programming.md", "specialImage":"/img/portfolio-3.jpg"},
+{"id": "item4", "fname":"database_dev.md", "specialImage":"/img/portfolio-4.jpg"}];
