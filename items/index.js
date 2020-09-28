badb8db56adb73051297e7c10149e3747f0143bf
Majid Tooranisama <mtooranisama5070@conestogac.on.ca>
2020-09-27 21:16:53 -0400

    Test7

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "fname":"/portfolio/items/website_dev.md", "specialImage":"/portfolio/img/portfolio-1.JPG"},
-{"id": "item2", "fname":"/portfolio/items/mobile_ui.md", "specialImage":"/portfolio/img/portfolio-2.JPG"},
-{"id": "item3", "fname":"/portfolio/items/c_programming.md", "specialImage":"/portfolio/img/portfolio-3.JPG"},
-{"id": "item4", "fname":"/portfolio/items/database_dev.md", "specialImage":"/portfolio/img/portfolio-4.JPG"}];
+export default [{"id": "item1", "fname":"website_dev.md", "specialImage":"/portfolio/img/portfolio-1.JPG"},
+{"id": "item2", "fname":"mobile_ui.md", "specialImage":"/portfolio/img/portfolio-2.JPG"},
+{"id": "item3", "fname":"c_programming.md", "specialImage":"/portfolio/img/portfolio-3.JPG"},
+{"id": "item4", "fname":"database_dev.md", "specialImage":"/portfolio/img/portfolio-4.JPG"}];
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 export default [
 {"title":"Index", "fname":"index.md"},
-{"title":"About", "fname":"/portfolio/pages/about.md", "specialImage":"/portfolio/img/about.JPEG"},
-{"title":"Contact", "fname":"/portfolio/pages/contact.md"}];
+{"title":"About", "fname":"about.md", "specialImage":"/portfolio/img/about.JPEG"},
+{"title":"Contact", "fname":"contact.md"}];
