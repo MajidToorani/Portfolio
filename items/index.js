5fff88eb6e523f6dbfe9f2edf7253cf90bdcb6b5
Majid Tooranisama <mtooranisama5070@conestogac.on.ca>
2020-09-27 20:45:06 -0400

    test2

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "fname":"website_dev.md", "specialImage":"..//img/portfolio-1.JPG"},
-{"id": "item2", "fname":"mobile_ui.md", "specialImage":"..//img/portfolio-2.JPG"},
-{"id": "item3", "fname":"c_programming.md", "specialImage":"..//img/portfolio-3.JPG"},
-{"id": "item4", "fname":"database_dev.md", "specialImage":"..//img/portfolio-4.JPG"}];
+export default [{"id": "item1", "fname":"website_dev.md", "specialImage":"/img/portfolio-1.JPG"},
+{"id": "item2", "fname":"mobile_ui.md", "specialImage":"/img/portfolio-2.JPG"},
+{"id": "item3", "fname":"c_programming.md", "specialImage":"/img/portfolio-3.JPG"},
+{"id": "item4", "fname":"database_dev.md", "specialImage":"/img/portfolio-4.JPG"}];
