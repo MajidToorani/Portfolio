d85464e532a9ea18353c1a60abeac70f7337d1bf
Majid Tooranisama <mtooranisama5070@conestogac.on.ca>
2020-09-27 21:58:06 -0400

    Test10

--- js/index.js
+++ js/index.js
@@ -5,7 +5,24 @@ import aPages from "../pages/index.js";
 import aItems from "../items/index.js";
 
 class Page {
-    render(){
+    constructor(){
+        this.sName = "Majid Tooranisama";
+        const sBase = document.location.pathname;
+        if(sBase[sBase.length - 1] == "/"){
+            this.sBase = sBase.substr(0, sBase.length -1);
+        }else{
+            const sFile = '/' + document.location.pathname.split('/').pop();
+            this.sBase = sBase.substr(0, sBase.length - sFile.length); 
+        }
+    }
+    getImageSrc(sImage){
+        if(sImage.match(/\:\/\//)){
+            return sImage;
+        }else{
+            return this.sBase + sImage;
+        }
+    }
+    render() {
         console.log("render called on page");
     }
 }
@@ -25,23 +42,24 @@ class Items extends Page{
     }
     render(){
         $("article#current").append(`
-            <img src="${this.oItems[this.nCurrentItem].specialImage}" />
+            <div class="itemImage"><img src="${this.getImageSrc(this.oItems[this.nCurrentItem].specialImage)}" /></div>
         `);
-        $.get(`/portfolio/items/${this.oItems[this.nCurrentItem].fname}`, (sMarkdown) => {
-            $("article#current").append(
-                marked(sMarkdown)
-            )
+        $.get(`${this.sBase}/items/${this.oItems[this.nCurrentItem].fname}`, (sMarkdown) => {
+            $("article#current").append(`
+                <div class="markdownItem">${marked(sMarkdown)}</div>
+            `)
 
         })
         for(let n = 0; n < this.oItems.length; n++){
             if(n != this.nCurrentItem){
                 $("article#items").append(`
                 <div class="item"><a class="itemLink" href="#">
-                <img id="item${n}" src="${this.oItems[n].specialImage}" /></a></div>
+                <img id="item${n}" src="${this.getImageSrc(this.oItems[n].specialImage)}" /></a></div>
                 `);
            }
         }
     }
+
 }
 
 class Section extends Page {
@@ -52,13 +70,14 @@ class Section extends Page {
     render() {
         if (this.oOptions.specialImage) {
             $(`#${this.oOptions.title}`).append(`
-            <img src="${this.oOptions.specialImage}" />
+            <div class="pageImage"><img src="${this.getImageSrc(this.oOptions.specialImage)}" /></div>
             `);
         }
-        $.get(`/portfolio/pages/${this.oOptions.fname}`, (sMarkdown) => {
-            $(`#${this.oOptions.title}`).append(
-                marked(sMarkdown)
-            )
+        $.get(`${this.sBase}/pages/${this.oOptions.fname}`, (sMarkdown) => {
+            $(`#${this.oOptions.title}`).append(`
+                <div class="markDownPage">${marked(sMarkdown)}</div>
+            `)
+
         })
     }
 }
@@ -74,13 +93,11 @@ class Article extends Page {
     }
 }
 
-const sName = "Majid Tooranisama";
-
 class Footer extends Page {
     render() {
         const yToday = new Date().getFullYear();
         $("footer").html(
-            `&copy; ${yToday} ${sName}`
+            `&copy; ${yToday} ${this.sName}`
         );
     }
 }
@@ -90,7 +107,7 @@ class Nav extends Page {
         let sMenu = "";
         for (let n = 0; n < aPages.length; n++) {
             const sMenuItem = aPages[n].title;
-            if (sMenuItem != "Index") {
+            if (sMenuItem != "index") {
                 sMenu += `<li><a href="#${sMenuItem}">${sMenuItem}</a></li>`;
             }
         }
@@ -104,7 +121,7 @@ class Nav extends Page {
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index">Portfolio of ${sName}</a>
+                <a class="navbar-brand" href="#">Portfolio of ${this.sName}</a>
             </div>
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
@@ -134,7 +151,148 @@ class Portfolio extends Page {
     }
 }
 
-$(document).ready(()=>{
+$(document).ready(() => {
     new Portfolio().render();
 });
+
+
+// import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js";
+// import "https://cdnjs.cloudflare.com/ajax/libs/marked/1.1.1/marked.js";
+// import "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js";
+// import aPages from "../pages/index.js";
+// import aItems from "../items/index.js";
+
+// class Page {
+//     render(){
+//         console.log("render called on page");
+//     }
+// }
+
+// class Items extends Page{
+//     constructor(oItems) {
+//         super();
+//         this.oItems = oItems;
+//         this.nCurrentItem = 0;
+//         $("article#items").click((evt) =>{
+//             evt.preventDefault();
+//             this.nCurrentItem = evt.target.id[4];
+//             $("article#current").html("");
+//             $("article#items").html("");
+//             this.render();
+//         });
+//     }
+//     render(){
+//         $("article#current").append(`
+//             <img src="${this.oItems[this.nCurrentItem].specialImage}" />
+//         `);
+//         $.get(`/portfolio/items/${this.oItems[this.nCurrentItem].fname}`, (sMarkdown) => {
+//             $("article#current").append(
+//                 marked(sMarkdown)
+//             )
+
+//         })
+//         for(let n = 0; n < this.oItems.length; n++){
+//             if(n != this.nCurrentItem){
+//                 $("article#items").append(`
+//                 <div class="item"><a class="itemLink" href="#">
+//                 <img id="item${n}" src="${this.oItems[n].specialImage}" /></a></div>
+//                 `);
+//            }
+//         }
+//     }
+// }
+
+// class Section extends Page {
+//     constructor(oOptions) {
+//         super();
+//         this.oOptions = oOptions;
+//     }
+//     render() {
+//         if (this.oOptions.specialImage) {
+//             $(`#${this.oOptions.title}`).append(`
+//             <img src="${this.oOptions.specialImage}" />
+//             `);
+//         }
+//         $.get(`/portfolio/pages/${this.oOptions.fname}`, (sMarkdown) => {
+//             $(`#${this.oOptions.title}`).append(
+//                 marked(sMarkdown)
+//             )
+//         })
+//     }
+// }
+
+// class Article extends Page {
+//     render() {
+//         for (let n = 0; n < aPages.length; n++) {
+//             $("article#pages").append(
+//                 `<section id="${aPages[n].title}"></section>`
+//             );
+//             new Section(aPages[n]).render();
+//         }
+//     }
+// }
+
+// const sName = "Majid Tooranisama";
+
+// class Footer extends Page {
+//     render() {
+//         const yToday = new Date().getFullYear();
+//         $("footer").html(
+//             `&copy; ${yToday} ${sName}`
+//         );
+//     }
+// }
+
+// class Nav extends Page {
+//     render() {
+//         let sMenu = "";
+//         for (let n = 0; n < aPages.length; n++) {
+//             const sMenuItem = aPages[n].title;
+//             if (sMenuItem != "Index") {
+//                 sMenu += `<li><a href="#${sMenuItem}">${sMenuItem}</a></li>`;
+//             }
+//         }
+
+//         $("nav").html(`
+//         <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+//             <div class="navbar-header">
+//                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+//                     <span class="sr-only">Toggle navigation</span>
+//                     <span class="icon-bar"></span>
+//                     <span class="icon-bar"></span>
+//                     <span class="icon-bar"></span>
+//                 </button>
+//                 <a class="navbar-brand" href="index">Portfolio of ${sName}</a>
+//             </div>
+//             <div class="navbar-collapse collapse">
+//                 <ul class="nav navbar-nav navbar-right">
+//                     ${sMenu}
+//                 </ul>
+//             </div>
+//         </div>
+//         `);
+//     }
+// }
+
+// class Portfolio extends Page {
+//     constructor() {
+//         super();
+//         this.header = new Page();
+//         this.nav = new Nav();
+//         this.items = new Items(aItems);
+//         this.article = new Article();
+//         this.footer = new Footer();
+//     }
+//     render() {
+//         this.header.render();
+//         this.nav.render();
+//         this.items.render();
+//         this.article.render();
+//         this.footer.render();
+//     }
+// }
+
+// $(document).ready(()=>{
+//     new Portfolio().render();
+// });
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 export default [
 {"title":"Index", "fname":"index.md"},
-{"title":"About", "fname":"about.md", "specialImage":"/portfolio/img/about.jpeg"},
+{"title":"About", "fname":"about.md", "specialImage":"/img/about.jpeg"},
 {"title":"Contact", "fname":"contact.md"}];
